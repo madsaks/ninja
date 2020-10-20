40fe6680516436be1e8704a578117fb69506f6be
aaron <arwchilds@gmail.com>
2020-10-20 16:36:02 +0800

    almost working

--- Javascript/typescript-project/src/App.tsx
+++ Javascript/typescript-project/src/App.tsx
@@ -1,187 +1,175 @@
-import React, { Dispatch, SetStateAction, useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
 
+interface List {
+  name: string;
+  items: Item[];
+}
 
+interface Item {
+  name: string;
+  checked: boolean;
+}
 
-  interface ItemProps {
-    name?: string
-    setItem: (item: IItem) => void
-    item: IItem
-    del: () => void
+// this should be its own component
+interface ListProps {
+  activeList: List
+  state: List[]
+  // Varible needs a name for some reason
+  setState: (state: List[]) => void
+  activeTab: string
+  isChecked: boolean 
+  setChecked: (arg0: boolean) => void
+}
 
-  }
+// List component ------------------------------------------------------------------------------------------------
+const List = (props: ListProps) => {
+  console.log("In listElement", props.activeList);
+  
+  
+  const itemDelHandler = (name: string) => {
+    console.log("itemDelHandler called");
 
-
-  function ShoppingListItem({ item, del, setItem }: ItemProps) {
-   
+    // Copies state into temp state
+    const tempState = props.state.map((l) => { return l });
+    // find index of list with name matching activeTab
+    const i = tempState.findIndex((i) => { return i.name === props.activeTab })
     
-    if (!item) {
-      return null
-    }
-    return (
-      <li >
-        {/* Item name */}
-        <span>
-          {item.checked ? <s>{item.name}</s> : item.name }
-        </span>
+    // need map function
+    const temp = tempState[i].items.filter(
+      (item) => {
+        item.name != name;
+
+      })
+    
+    props.setState(tempState)
+
+  };
+
+  const checkHandler = (name: string) => {
+    var tempState = props.state.map((l: any) => { return l });
+    // find index of list with name matching activeTab
+    const i = tempState.findIndex((i: any) => { return i.name === props.activeTab })
+
+    const i2 = tempState[i].items.findIndex((i: any) => { return i.name === name })
+
+    tempState[i].items[i2].checked = !tempState[i].items[i2].checked;
+
         
-        {/* Delete button */}
-        <span>
-          <button
-            onClick={() => { del() }}>Delete
-          </button>
-          {/* Need to add item.checked function */}
-          <input type="checkbox" checked={item.checked} onChange={() => {
-            setItem({
-              ...item,
-              checked: !item.checked
-            })
-          }}/>
-        </span>
-      </li>)
-  }
-
-
-  interface ListProps {
-    items: IItem[]
-    setItems: (value: IItem[]) => void
-  }
-
-
-
-
-function ShoppingList(props: ListProps) {
-    const {setItems, items} = props
-    return (
-      <ul className="shopping-list">
-        {items.map(function (item: IItem) {
-          return <ShoppingListItem
-            // key={name}
-            item={item}
-            setItem={(newItem: IItem) => {
-              const index = items.indexOf(item)
-              let newItems = [...items]
-              newItems[index] = newItem
-              setItems(newItems)
-            }}
-            del={() => {
-              const updatedArray = props.items.filter((val) => {
-                return item !== val
-              })
-            props.setItems(updatedArray)
-            }}        
-          
-          />
-        })}
-      </ul>
-    )
-  }
-
-interface IItem {
-  name: string,
-  checked: boolean
-}
-
-interface ISList {
-  
-  setItems: (value: IItem[] ) => void
-  setNewItem: (value: string) => void
-  newItem: string
-  items: IItem[]
-}
-
-interface TabProps{
-  setActiveTab: (value: ISList[]) => void
-  getNewList: (value: ISList) => void
-  tabIndex?: BigInt
-  tabData?: ISList[]
-}
-
-
-function SList(props : TabProps) {
-
-  return (
-    // Need code to replicate SList in the same way that shopping list was
-    // {items.map(function (item: IItem) {
-    //   return <ShoppingListItem
-    //     // key={name}
-    //     item={item}
-    //     setItem={(newItem: IItem) => {
-    //       const index = items.indexOf(item)
-    //       let newItems = [...items]
-    //       newItems[index] = newItem
-    //       setItems(newItems)
-    //     }}
-    //     del={() => {
-    //       const updatedArray = props.items.filter((val) => {
-    //         return item !== val
-    //       })
-    //     props.setItems(updatedArray)
-    //     }}        
     
-    props.tabData?.map(function (tab: TabProps)) {
-      <div className="flexbox" id="app">
-        <div className="shoppingList">
-          <h1>Shopping List</h1>
-          <div className="input-wrap">
-
-            <input className="textInput" value={props.tabData.newItem} onChange={(e) => {
-              props.tabData.setNewItem(e.currentTarget.value);
-            }} />
-
-            <button className="deleteButton" onClick={() => {
-              // Changed as it was passing function instead of array
-              props.tabData.setItems([...props.tabData.items, { name: props.tabData.newItem, checked: false }])
-            }}>Add</button>
-          </div>
-
-          <ShoppingList items={props.tabData.items} setItems={props.tabData.setItems} />
-        </div>
-      </div>
+    // Update checked
+    // tempState[i].items.
+    props.setState(tempState)
+    
   }
-  )
-}
-
-function App() {
-  //  Define our state holding variables
-      
-  const [newItem, setNewItem] = useState("")
-  const [items, setItems] = useState<IItem[]>([
-    
-    { name: "Bread", checked: false },
-    { name: "Milk", checked: false },
-    { name: "Cheese", checked: false }
-  ])
-  const [list, setActiveTab] = useState<ISList[]>()
-  const [newList, getNewList] = useState<ISList>()
-
-  
-    
-    // Begin main app
-    
   return (
     <div>
-      
-
-      {/* Default one list */}
-      
-      
-    
+      {props.activeList.items.map((el) => {
         
-      {/* <button onClick={setList}></button> */}
-      <SList setItems={setItems} setNewItem={setNewItem} newItem={newItem} items={items} />
-      <SList setItems={setItems} setNewItem={setNewItem} newItem={newItem} items={items} />
-
+        let item: any;
+        el.checked ? item = <s>{el.name}</s> : item = el.name
       
+        
+        return (
+          <li>
+            {item}
+            {/* If eventhandler onChange used then this is run on all lists */}
+            <input type="checkbox" checked={el.checked} onClick={() => { checkHandler(el.name) }} />
+            <button onClick={() => { itemDelHandler(el.name) }}>Del</button>
+          </li>
+        );
+      })}
     </div>
-    );
-}
+  );
+};
 
+// Main app entry -----------------------------------------------------------------------------------------------------------
+const App = () => {
+  // get checked items name
+  const [isChecked, setChecked] = useState<boolean>(false);
+  const [newList, setNewList] = useState<string>("New Tab");
+  const [activeTab, setActiveTab] = useState<string>("List 1");
+  const [newItem, setNewItem] = useState<string>("");
+  const [state, setState] = useState<List[]>([
+    {
+      name: "List 1",
+      items: [{ name: "milk", checked: false }]
+    },
+    {
+      name: "List 2",
+      items: [{ name: "bread", checked: true }]
+    }
+  ]);
+ 
 
+  const activeList = state.find((el) => {
+    return el.name === activeTab;
+  });
 
-  export default App;
+  const addListToState = () => {
+    let newState = state.map((el) => {
+      return el;
+    });
+    newState.push({ name: newList, items: [] });
+    setState(newState);
+  };
+  if (!activeList) {
+    return <div>No list</div>;
+  }
 
+  // Take (item) and add to current list (activeList)
+  const addNewListItem = () => {
+    console.log("Inside addNewListItem, activeList = ", activeList);
+    // Copies state into temp state
+    const tempState = state.map((l) => { return l });
+    // find index of list with name matching activeTab
+    const i = tempState.findIndex((i) => { return i.name === activeTab })
+    
+    // Push 
+    tempState[i].items.push({ name: newItem, checked: false });
+    setState(tempState)
+    console.log(newItem);
+    
+  };
 
+  return (
+    <div>
+      {/* Display List Buttons */}
+      {state.map((tab) => (
+        <button
+          onClick={(e) => {
+            setActiveTab(tab.name);
+          }}
+        >
+          {tab.name}
+        </button>
+      ))}
+      {/* New List Button */}
+      {/* how did you know to use on change on input form and not via click */}
+      <input
+        type="text"
+        onChange={(e) => {
+          setNewList(e.currentTarget.value);
+        }}
+      />
+      <button onClick={addListToState}>New List</button>
 
+      <h1>{activeList.name}</h1>
 
+      {/* Add new Item to list */}
+      <input
+        type="text"
+        onChange={(e) => {
+          setNewItem(e.currentTarget.value);
+        }}
+      />
+      <button onClick={addNewListItem}>add</button>
 
+      {/* Begin List */}
+      <ul>
+        <List state={state} setState={setState} activeList={activeList} activeTab={activeTab} isChecked={isChecked} setChecked={setChecked} />
+      </ul>
+    </div>
+  );
+};
+
+export default App;
